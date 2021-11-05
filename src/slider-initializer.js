448af4c8fae53178e60b12daf97060d254fd6a2a
juniorjmm <josemjunior17@gmail.com>
2021-11-05 15:40:42 -0400

    fix no script do slider

--- src/slider-initializer.js
+++ src/slider-initializer.js
@@ -1,36 +1,42 @@
 window.addEventListener('load', function () {
-    if (!Glider) return;
+    const editor = document.getElementById('editor');
 
-    const sliders = document.querySelectorAll('.glider');
+    function createSliders() {
+        if (!Glider) return;
 
-    for (const slider of sliders) {
-        const autoOrValue = (v) => v === 'auto' ? v : isNaN(Number(v)) ? undefined : Number(v);
-        const parseOptions = (v) => {
-            try {
-                return JSON.parse(v);
-            } catch (e) {
-                return undefined;
+        const sliders = document.querySelectorAll('.glider');
+
+        for (const slider of sliders) {
+            const autoOrValue = (v) => v === 'auto' ? v : isNaN(Number(v)) ? undefined : Number(v);
+            const parseOptions = (v) => {
+                try {
+                    return JSON.parse(v);
+                } catch (e) {
+                    return undefined;
+                }
             }
+            const { slidesToShow, slidesToScroll, itemWidth, exactWidth, duration, responsive } = slider.dataset;
+
+            new Glider(slider, {
+                slidesToShow: autoOrValue(slidesToShow) ?? 1,
+                slidesToScroll: autoOrValue(slidesToScroll) ?? 1,
+                duration: Number(duration) ?? 0.5,
+                responsive: parseOptions(responsive),
+                draggable: true,
+                dots: '.dots',
+                arrows: {
+                    prev: '.glider-prev',
+                    next: '.glider-next'
+                },
+
+                itemWidth,
+                exactWidth,
+            });
         }
-        const { slidesToShow, slidesToScroll, itemWidth, exactWidth, duration, responsive } = slider.dataset;
-
-        console.log(autoOrValue(slidesToShow) ?? 1)
-        console.log(autoOrValue(slidesToScroll) ?? 1)
-
-        new Glider(slider, {
-            slidesToShow: autoOrValue(slidesToShow) ?? 1,
-            slidesToScroll: autoOrValue(slidesToScroll) ?? 1,
-            duration: Number(duration) ?? 0.5,
-            responsive: parseOptions(responsive),
-            draggable: true,
-            dots: '.dots',
-            arrows: {
-                prev: '.glider-prev',
-                next: '.glider-next'
-            },
-
-            itemWidth,
-            exactWidth,
-        });
     }
+
+    // Para criar sliders no editor toda vez que o bloco mudar da edição (onde mostra apenas os inputs) para a visualização
+    if (editor) editor.addEventListener('DOMSubtreeModified', createSliders);
+
+    createSliders();
 })
